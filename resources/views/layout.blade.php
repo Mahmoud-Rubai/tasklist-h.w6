65c5b1ded0e1ca81450569db16188df22d5572ab
Mahmoud-Rubaigit config --global user.name Mahmoud-Rubaigit config --global user.name Mahmoud-Rubai <mahmoudrab1999@gmail.com>
2021-05-06 23:17:44 +0300

    first commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,9 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <u1>
-    @foreach ($tasks as $task)
-    <li><a href="tasks/show/{id}">{{$task->title}}</a><li>
-  @foreach
+   @yield('content')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,66 +1,39 @@
-<?php
-
-
-//use GuzzleHttp\Psr7\Request;
+?php
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('about', function () {
-    $name = "Mahmoud AlRubai";
-    return view('about', compact('name'));
+Rout::get('task', function(){
+    $tasks = DB::table('tasks')->get();
+    //dd($tasks);
+    return view('tasks');
 });
-
-
-Route::post('store', function (Request $request) {
-    $name = $request->myname;
-    return view('about', compact('name'));
-});
-
-//************** */
-
-Route::get('tasks', function () {
-    $tasks = [
-        'first-task' => 'task 1',
-        'second-task' => 'task 2',
-        'third-task' => 'task 3'
-    ];
-
-    return view('tasks', compact('tasks'));
-});
-
-Route::get('show/{id}', function ($id) {
-    $tasks = [
-        'first-task' => 'task 1',
-        'second-task' => 'task 2',
-        'third-task' => 'task 3'
-    ];
-    $task = $tasks[$id];
-
+Route::get('tasks/show/{id}', function($id){
+    $task = DB::table('tasks')->where('id',$id)->find($id);
+    dd($task);
     return view('show', compact('task'));
 });
-
-//************** */
-
-Route::get('tasks',function(){
-
-    $tasks= DB::table('tasks')->get();
-    //dd($task);
-
-
-    return view('tasks',compact('tasks'));
+Route::get('app', function(){
+    $tasks = DB::table('tasks')->get();
+    return view('todo', compact('tasks'));
 });
-
-Route::get('tasks/show/{id}' , function($id){
-
-    $task= DB::table('tasks')->find($id);
-    //dd($task);
-
-    return view('show',compact('task'));
-
+Rout::post('store', function(Request $request){
+    DB::table('tasks')->insert([
+     'title' => $request->title
+    ]);
+    return redirect()->back();
 });
